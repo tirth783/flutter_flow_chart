826c5e65d5c65104b787d20527f67630a9f582f8
km_and01 <koolmind.and01@gmail.com>
2025-06-17 17:31:30 +0530

    pkg changes

--- lib/src/objects/element_text_widget.dart
+++ lib/src/objects/element_text_widget.dart
@@ -1,77 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_flow_chart/flutter_flow_chart.dart';
+import 'package:flutter_flow_chart/src/elements/flow_element.dart';
 
 /// Common widget for the element text
-class ElementTextWidget extends StatefulWidget {
+class ElementTextWidget extends StatelessWidget {
   ///
   const ElementTextWidget({
-    required this.element,
     super.key,
+    required this.text,
+    required this.textColor,
+    required this.textSize,
+    this.fontFamily,
+    this.isBold = false,
   });
 
   ///
-  final FlowElement element;
-
-  @override
-  State<ElementTextWidget> createState() => _ElementTextWidgetState();
-}
-
-class _ElementTextWidgetState extends State<ElementTextWidget> {
-  final _controller = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller
-      ..text = widget.element.text
-      ..addListener(() => widget.element.text = _controller.text);
-  }
+  final String text;
+  final Color textColor;
+  final double textSize;
+  final String? fontFamily;
+  final bool isBold;
 
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      color: widget.element.textColor,
-      fontSize: widget.element.textSize,
-      fontWeight: widget.element.textIsBold ? FontWeight.bold : FontWeight.normal,
-      fontFamily: widget.element.fontFamily,
+    return Text(
+      text,
+      style: TextStyle(
+        color: textColor,
+        fontSize: textSize,
+        fontFamily: fontFamily,
+        fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+      ),
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
     );
-
-    final subTextStyle = TextStyle(
-      color: widget.element.subTextColor,
-      fontSize: widget.element.subTextSize,
-      fontFamily: widget.element.fontFamily,
-    );
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Align(
-          child: widget.element.isEditingText
-              ? TextFormField(
-                  controller: _controller,
-                  autofocus: true,
-                  onTapOutside: (event) => dismissTextEditor(),
-                  onFieldSubmitted: dismissTextEditor,
-                  textAlign: TextAlign.center,
-                  style: textStyle,
-                )
-              : Text(
-                  widget.element.text,
-                  textAlign: TextAlign.center,
-                  style: textStyle,
-                ),
-        ),
-        Text(
-          widget.element.subText,
-          textAlign: TextAlign.center,
-          style: subTextStyle,
-        )
-      ],
-    );
-  }
-
-  void dismissTextEditor([String? text]) {
-    if (text != null) widget.element.text = text;
-    setState(() => widget.element.isEditingText = false);
   }
 }
--- lib/src/objects/image_widget.dart
+++ lib/src/objects/image_widget.dart
@@ -16,16 +16,18 @@ class ImageWidget extends StatefulWidget {
   })  : assert(
           element.data is ImageProvider ||
               (element.serializedData?.isNotEmpty ?? false),
-          'Missing image ("data" parameter should be an ImageProvider)',
+          'Missing image (\"data\" parameter should be an ImageProvider)',
         ),
         imageProvider = element.serializedData?.isNotEmpty ?? false
-            ? Image.memory(base64Decode(element.serializedData!)).image
+            ? (element.serializedData!.startsWith('http')
+                ? NetworkImage(element.serializedData!)
+                : Image.memory(base64Decode(element.serializedData!)).image)
             : element.data as ImageProvider;
 
-  ///
+  /// The element to display
   final FlowElement element;
 
-  /// The image to render
+  /// The image provider
   final ImageProvider imageProvider;
 
   @override
@@ -33,84 +35,42 @@ class ImageWidget extends StatefulWidget {
 }
 
 class _ImageWidgetState extends State<ImageWidget> {
-  ui.Image? _cachedImage;
-  String? _error;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    debugPrint('Rendering ImageWidget of size ${widget.element.size} '
-        'from provider ${widget.imageProvider.runtimeType} '
-        'and cachedImage $_cachedImage');
-
-    final w = widget.element.size.width;
-    final h = widget.element.size.height;
-
-    final double diameter = math.min(w * 0.8, h * 0.6);
-
-    return SizedBox(
-      width: w,
-      height: h,
-      child: Stack(
-        children: [
-          // Background + border
-          Container(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+          child: Container(
             decoration: BoxDecoration(
-              color: widget.element.backgroundColor,
-              borderRadius: BorderRadius.circular(8),
               border: Border.all(
                 color: widget.element.borderColor,
                 width: widget.element.borderThickness,
               ),
             ),
+            child: Image(
+              image: widget.imageProvider,
+              fit: BoxFit.cover,
+            ),
           ),
-
-          // Image + text
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Circular image
-              Expanded(
-                flex: 3,
-                child: Center(
-                  child: ClipOval(
-                    child: Image(
-                      image: widget.imageProvider,
-                      width: diameter,
-                      height: diameter,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          width: diameter,
-                          height: diameter,
-                          decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(Icons.person,
-                              size: diameter * 0.5, color: Colors.grey[600]),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-
-              // Text beneath
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: ElementTextWidget(element: widget.element),
-                ),
-              ),
-            ],
+        ),
+        if (widget.element.text.isNotEmpty)
+          ElementTextWidget(
+            text: widget.element.text,
+            textColor: widget.element.textColor,
+            textSize: widget.element.textSize,
+            fontFamily: widget.element.fontFamily,
+            isBold: widget.element.textIsBold,
           ),
-        ],
-      ),
+        if (widget.element.subText.isNotEmpty)
+          ElementTextWidget(
+            text: widget.element.subText,
+            textColor: widget.element.subTextColor,
+            textSize: widget.element.subTextSize,
+            fontFamily: widget.element.fontFamily,
+            isBold: widget.element.textIsBold,
+          ),
+      ],
     );
   }
 }
