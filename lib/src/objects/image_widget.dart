58ec0787cd5ee2ffceb34e72376ec9d2bc316424
km_and01 <koolmind.and01@gmail.com>
2025-06-18 10:05:04 +0530

    pkg changes

--- lib/src/objects/image_widget.dart
+++ lib/src/objects/image_widget.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'dart:math' as math;
+import 'dart:ui' as ui;
+import 'dart:io' show File;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_flow_chart/src/elements/flow_element.dart';
@@ -12,8 +14,9 @@ class ImageWidget extends StatefulWidget {
     required this.element,
     super.key,
   })  : assert(
-          element.data is ImageProvider || (element.serializedData?.isNotEmpty ?? false),
-          'Missing image (\"data\" parameter should be an ImageProvider)',
+          element.data is ImageProvider ||
+              (element.serializedData?.isNotEmpty ?? false),
+          'Missing image ("data" parameter should be an ImageProvider)',
         ),
         imageProvider = element.serializedData?.isNotEmpty ?? false
             ? (element.serializedData!.startsWith('http')
@@ -32,53 +35,90 @@ class ImageWidget extends StatefulWidget {
 }
 
 class _ImageWidgetState extends State<ImageWidget> {
+  ui.Image? _cachedImage;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint('Rendering ImageWidget of size ${widget.element.size} '
+        'from provider ${widget.imageProvider.runtimeType} '
+        'and cachedImage $_cachedImage');
+
     final w = widget.element.size.width;
     final h = widget.element.size.height;
 
     final double diameter = math.min(w * 0.8, h * 0.6);
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Expanded(
-          child: Container(
+    return SizedBox(
+      width: w,
+      height: h,
+      child: Stack(
+        children: [
+          // Background + border
+          Container(
             decoration: BoxDecoration(
-              color: Colors.white,
+              color: widget.element.backgroundColor,
+              borderRadius: BorderRadius.circular(8),
               border: Border.all(
                 color: widget.element.borderColor,
                 width: widget.element.borderThickness,
               ),
             ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image(
-                image: widget.imageProvider,
-                fit: BoxFit.cover,
-                width: diameter,
-                height: diameter,
+          ),
+
+          // Image + text
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Circular image
+              Expanded(
+                flex: 3,
+                child: Center(
+                  child: ClipOval(
+                    child: Image(
+                      image: widget.imageProvider,
+                      width: diameter,
+                      height: diameter,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          width: diameter,
+                          height: diameter,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(Icons.person,
+                              size: diameter * 0.5, color: Colors.grey[600]),
+                        );
+                      },
+                    ),
+                  ),
+                ),
               ),
-            ),
+
+              // Text beneath
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: ElementTextWidget(
+                    text: widget.element.text,
+                    textColor: widget.element.textColor,
+                    textSize: widget.element.textSize,
+                    fontFamily: widget.element.fontFamily,
+                    isBold: widget.element.textIsBold,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
-        if (widget.element.text.isNotEmpty)
-          ElementTextWidget(
-            text: widget.element.text,
-            textColor: widget.element.textColor,
-            textSize: widget.element.textSize,
-            fontFamily: widget.element.fontFamily,
-            isBold: widget.element.textIsBold,
-          ),
-        if (widget.element.subText.isNotEmpty)
-          ElementTextWidget(
-            text: widget.element.subText,
-            textColor: widget.element.subTextColor,
-            textSize: widget.element.subTextSize,
-            fontFamily: widget.element.fontFamily,
-            isBold: widget.element.textIsBold,
-          ),
-      ],
+        ],
+      ),
     );
   }
 }
